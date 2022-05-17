3e2adbb3e209740edbfa8b300b5affa92afdc7de
devtopher <christopher.david.mccoy@gmail.com>
2022-05-17 12:43:49 -0400

    option update

--- src/components/TaskModule.vue
+++ src/components/TaskModule.vue
@@ -2,6 +2,16 @@
     <div>
         <label for="hour-select">Choose an hour:</label><select name="hours" id="hour-select">
             <option value="8">8AM</option>
+            <option value="9">9AM</option>
+            <option value="10">10AM</option>
+            <option value="11">11AM</option>
+            <option value="12">12PM</option>
+            <option value="13">1PM</option>
+            <option value="14">2PM</option>
+            <option value="15">3PM</option>
+            <option value="16">4PM</option>
+            <option value="17">5PM</option>
+            <option value="18">6PM</option>
             </select>
         <textarea></textarea>
         <Button buttonText="Submit New Task"  />
